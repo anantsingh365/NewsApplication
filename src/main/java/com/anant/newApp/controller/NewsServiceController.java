bec158bff833c25b320f5171c9774e0030f5b7ef
Anant Singh <sanantsingh365@gmail.com>
2022-08-26 12:51:58 +0530

    newListingPage

--- src/main/java/com/anant/newApp/controller/NewsServiceController.java
+++ src/main/java/com/anant/newApp/controller/NewsServiceController.java
@@ -1,13 +1,8 @@
 package com.anant.newApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
-import java.util.Map;
-
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 

ce7307c222a8a63acfedf1493c66213b42b6b740
Anant Singh <sanantsingh365@gmail.com>
2022-09-19 10:55:26 +0530

    double quote remove from newsListin.js

--- src/main/resources/static/js/newListing.js
+++ src/main/resources/static/js/newListing.js
@@ -1,5 +1,4 @@
-function saveArticle(clicked_id)
-{
+function saveArticle(clicked_id){
         let saveArticleUrl= "http://192.168.1.6:10000/" + "saveArticle?id=" + clicked_id;
         console.log(saveArticleUrl);
         let xmlHttpReq = new XMLHttpRequest();
@@ -7,6 +6,7 @@ function saveArticle(clicked_id)
          xmlHttpReq.send(null);
          console.log(xmlHttpReq.responseText);
 }
+
 function deleteArticle(clicked_element){
     let url = clicked_element.getAttribute('url');
     let deleteArticleUrl = "http://192.168.1.6:10000/deleteArticle";
@@ -21,9 +21,9 @@ function deleteArticle(clicked_element){
         console.log(xhr.responseText);
       }};
 
-    let data = "{
+    let data = {
      "url":url
-    }";
+    };
 
     xhr.send(data);
     console.log(xhr.responseText);
